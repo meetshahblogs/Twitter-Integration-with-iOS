7f98e834e35d41c57e89ca072557257af3b33886
Meet <meet86@gmail.com>
2018-01-15 17:15:12 -0600

    Implemented TwitterLoginButton integration with successful login valication and call-back.

--- TwitterIntegration/App Delegate/AppDelegate.m
+++ TwitterIntegration/App Delegate/AppDelegate.m
@@ -30,6 +30,9 @@
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
+    return [[Twitter sharedInstance] application:app openURL:url options:options];
+}
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
